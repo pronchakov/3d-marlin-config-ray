9296d6cdb2ebef9389af3cbd7917f5f3160509ab
Artem Pronchakov <artem.pronchakov@gmail.com>
2023-10-14 22:34:31 +0300

    set calculated extruder resolution, fix Y-offset from nozzle to 3d-touch

--- Configuration.h
+++ Configuration.h
@@ -1168,7 +1168,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 40, 40, 200, 212.45 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 40, 40, 200, 207.26 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
@@ -1483,7 +1483,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 33, 21, -1.4 }
+#define NOZZLE_TO_PROBE_OFFSET { 33, -21, -1.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
